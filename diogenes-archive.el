1c4c56c6f30fcc30d8760d46f81cd1aed74ff6b4
Victor Gonçalves de Sousa <victor2971@gmail.com>
2026-09-18 06:18:46 -0400

    give every file a copyright line and the GPL notice

--- diogenes-archive.el
+++ diogenes-archive.el
@@ -1,3 +1,21 @@
+;;; diogenes-archive.el --- FIXME: one-line summary -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2026 Victor Gonçalves de Sousa
+;;
+;; Author: Victor Gonçalves de Sousa <victor.goncalves.sousa@alumni.usp.br>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 (defun diogenes--linear-search (dict-file comp-fn key-fn word &optional start stop)
   "A linear search for finding entries in the lexicographical files.
 Returns a list of all entries where comp-fn returned nil."
--- diogenes-presets.el
+++ diogenes-presets.el
@@ -3,6 +3,23 @@
 ;; Author: Victor Sousa
 ;; Keywords: languages, tools
 
+;; Copyright (C) 2026 Victor Gonçalves de Sousa
+;;
+;; Author: Victor Gonçalves de Sousa <victor.goncalves.sousa@alumni.usp.br>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; A preset is an ordinary Emacs Lisp file that sets Diogenes options.  There
